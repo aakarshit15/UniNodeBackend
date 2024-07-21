d26828fc762cfa7d7b17f9bc5d78184e7e237728
Aakarshit <saxenaakarshit15@gmail.com>
2024-07-21 15:13:07 +0530

    Add README.md and remove spelling mistake

---
+++ Task1/README.md
@@ -0,0 +1,28 @@
+
+# Letter Counting
+
+This project is for DJS Unicode interview task. It takes input as a text and outputs the number of occurrences of each letter
+
+
+
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  git clone https://github.com/aakarshit15/UniNodeBackend.git
+```
+
+Then install npm packages inside the project using
+
+```bash
+npm i
+```
+
+And then run the project using
+
+```bash
+node index.js
+```
+
--- Task1/index.js
+++ Task1/index.js
@@ -11,7 +11,7 @@ inquirer
   .then((answers) => {
     // Answer
     const text = answers.text; // answers = {text: <user input>}
-    let outputs = [];  // to store output for each letter => [{letter: <letter>, occurunces: <occurunces>}, ...]
+    let outputs = [];  // to store output for each letter => [{letter: <letter>, occurrences: <occurrences>}, ...]
     let alreadyCounted; // to not repeat a letter in outputs array
     for(let i=0; i<text.length; i++) {
       if(text[i] === " ") {
@@ -20,33 +20,33 @@ inquirer
       alreadyCounted = false; // initially letter is not counted
       outputs.forEach((output) => { // inside for each loop to see if letter is counted
         if(output.letter === text[i]) { // if counted then
-          output.occurunces++; // increase its occurence
+          output.occurrences++; // increase its occurrence
           alreadyCounted = true; // set alreadyCounted true
           return; // get out of for each loop
         }
       });
-      !alreadyCounted && outputs.push({letter: text[i], occurunces: 1}); // if not counted then count it for the first time
+      !alreadyCounted && outputs.push({letter: text[i], occurrences: 1}); // if not counted then count it for the first time
     }
     /*
       after for loop
       outputs = [
         {
           letter: <single letter>
-          occurunces: <number of occurunces of that letter>
+          occurrences: <number of occurrences of that letter>
         }, ...
       ]
     */
     console.log(outputs.length); //to print number of distinct letters
-    let outputOccurunces = "", outputLetterOccurunces = "";
+    let outputOccurrences = "", outputLetterOccurrences = "";
     outputs.forEach((output) => { // for each letter (or output) in outputs array
-      outputOccurunces += `${output.occurunces} `; // add its occurunces into ouputOccurunces string
+      outputOccurrences += `${output.occurrences} `; // add its occurrences into outputOccurrences string
     });
     outputs.forEach((output, index) => { // for each letter (or output) in outputs array
-      (index !== 0) && (outputLetterOccurunces += ", "); // if not 1st output then add a comma and a space before next letter
-      outputLetterOccurunces += `${output.letter}:${output.occurunces}` // add its letter & occurunces into ouputOccurunces string
+      (index !== 0) && (outputLetterOccurrences += ", "); // if not 1st output then add a comma and a space before next letter
+      outputLetterOccurrences += `${output.letter}:${output.occurrences}` // add its letter & occurrences into ouputOccurunces string
     });
-    console.log(outputOccurunces); // to print each occurunces
-    console.log(outputLetterOccurunces); // to print each letter:occurunces
+    console.log(outputOccurrences); // to print each occurrences
+    console.log(outputLetterOccurrences); // to print each letter:occurrences
   })
   .catch((err) => {
     // Error handling
